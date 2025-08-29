5f9c3d098656e3259883e71bc76bed9fe93b9761
Manasi S <manasimvs11@gmail.com>
2025-08-29 17:26:19 +0530

    Completed till TASK 1C

--- task_1c.py
+++ task_1c.py
@@ -12,6 +12,29 @@ bestX = -1
 bestY = -1
 
 # to check for the bomb centres
-for y in range (radius, n-radius):
-    for x in range (radius, n-radius):
-        i=0
+for x in range (radius, n-radius):
+    for y in range (radius, n-radius):
+        
+        #the bomb can detonate only on land
+        if grid[x][y] == 1:
+            count_land = 0
+            for i in range (x-radius, x+radius+1):
+                for j in range (y-radius, y+radius+1):
+                    if grid[i][j] == 1:
+                        count_land += 1
+            
+            if count_land > maxLand:
+                maxLand = count_land
+                bestX = x
+                bestY = y
+
+if bestX == -1:
+    print('BOMB DID NOT DETONATE!')
+
+else:
+    Y = n-bestY
+    print(f'Best place to drop the bomb: {bestX}, {Y}')
+    print('Destroyed cells: ')
+    for i in range (bestX-radius, bestX+radius+1):
+        for j in range (bestY-radius, bestY+radius+1):
+            print(f'({i}, {n-j})', end=' ')
